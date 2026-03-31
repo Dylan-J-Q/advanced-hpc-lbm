eeffb12e3f8614a98d322ba5c778a9fe353ba480
Dylan Quinton <47297648+Dylan-J-Q@users.noreply.github.com>
2026-03-31 18:30:05 +0100

    Update job.sh

--- job.sh
+++ job.sh
@@ -1,13 +1,10 @@
 #!/bin/bash
 #SBATCH --nodes=1
-#SBATCH --time=00:10:00
+#SBATCH --time=02:00:00
 #SBATCH --cpus-per-task=1
-#SBATCH --exclusive 
 #SBATCH --ntasks-per-node=1
 
 
 make
-./d2q9-bgk input_128x128.params obstacles_128x128.dat
-./d2q9-bgk input_128x256.params obstacles_128x256.dat
-./d2q9-bgk input_256x256.params obstacles_256x256.dat
-./d2q9-bgk input_1024x1024.params obstacles_1024x1024.dat
+./d2q9-bgk "$PROJECTDIR/public/d2q9-bgk-inputs/input_2048x2048.params" "$PROJECTDIR/public/d2q9-bgk-inputs/obstacles_2048x2048.dat"
+./d2q9-bgk "$PROJECTDIR/public/d2q9-bgk-inputs/input_4096x4096.params" "$PROJECTDIR/public/d2q9-bgk-inputs/obstacles_4096x4096.dat"
